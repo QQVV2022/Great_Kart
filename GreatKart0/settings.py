0b4c95fd4f51e70cc182f3b2be8b34c3f67db24d
Quinn <88351759+QQVV2022@users.noreply.github.com>
2022-04-26 16:02:50 -0700

    aws eb & S3

--- GreatKart0/settings.py
+++ GreatKart0/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'GreatKart0.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.mysql',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
@@ -107,14 +107,9 @@ if 'RDS_DB_NAME' in os.environ:
 else:
     DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lmall',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT':3306,
-        # 'NAME': 'GreatKart0' # db name
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 CACHES = {
